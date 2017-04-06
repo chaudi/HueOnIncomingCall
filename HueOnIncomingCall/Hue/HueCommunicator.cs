7c237564f302ec92cb1de2cb711ae53016821c79
ludvig <cederfelt@live.com>
2017-04-06 19:39:27 +0200

    Some rough work on communication

---
+++ HueOnIncomingCall/Hue/HueCommunicator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using HueOnIncomingCall.Hue.Messages;
+
+namespace HueOnIncomingCall.Hue
+{
+    public class HueCommunicator
+    {
+        private HttpClient client;
+
+        public HueCommunicator(string ip)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(ip);
+        }
+
+        public async Task<bool> SetLights(IEnumerable<Light> lights)
+        {
+            var json = JsonConvert.SerializeObject(lights);
+            var content = new StringContent(json);
+
+            var response = await client.PostAsync("/api/lights", content);
+            var responseMessage = await response.Content.ReadAsStringAsync();
+
+
+            return responseMessage.Contains("error");
+        }
+
+        public async Task<RegisteredResponse> RegisterApp()
+        {
+            var content = new StringContent("");
+            var response = await client.PostAsync("/api/register/", content);
+            var responseObject = JsonConvert.DeserializeObject<RegisteredResponse>(await response.Content.ReadAsStringAsync());
+            return responseObject;
+        }
+
+    }
+}
---
+++ HueOnIncomingCall/Hue/Light.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HueOnIncomingCall.Hue
+{
+    public class Light
+    {
+        public string Id { get; set; }
+        public int[] XY { get; set; }
+        public string Name { get; set; }
+
+        public Light()
+        {
+            XY = new int[2];
+        }
+    }
+}
