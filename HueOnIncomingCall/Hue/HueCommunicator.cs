4fc58d6b75bafc4a7870117d4e0c72e27d496d43
ludvig <cederfelt@live.com>
2017-04-28 18:52:35 +0200

    Worked on more calls to hue

--- HueOnIncomingCall/Hue/HueCommunicator.cs
+++ HueOnIncomingCall/Hue/HueCommunicator.cs
@@ -1,48 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using HueOnIncomingCall.Hue.Messages;
+using HueOnIncomingCall.Hue.Messages.Lights;
 
 namespace HueOnIncomingCall.Hue
 {
     public class HueCommunicator
     {
         private HttpClient client;
+        public string Username { get; set; }
 
         public HueCommunicator(string ip)
         {
-            client = new HttpClient();
-            client.BaseAddress = new Uri(ip);
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri(ip)
+            };
+        }
+
+        public async Task<GetLightsResponse> GetLights()
+        {
+
+            var response = await client.GetAsync($"/api/{Username}/lights");
+            var responseMessage = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonConvert.DeserializeObject<GetLightsResponse>(await response.Content.ReadAsStringAsync());
+
+            return responseObject;
         }
 
         public async Task<bool> SetLights(IEnumerable<Light> lights)
         {
-            var json = JsonConvert.SerializeObject(lights);
-            var content = new StringContent(json);
+            foreach (var light in lights)
+            {
+                var json = JsonConvert.SerializeObject(lights);
+                var content = new StringContent(json);
 
-            var response = await client.PostAsync("/api/lights", content);
-            var responseMessage = await response.Content.ReadAsStringAsync();
-
-
-            return responseMessage.Contains("error");
+                var response = await client.PutAsync($"/api/{Username}/lights{light.Id}", content);
+                var responseMessage = await response.Content.ReadAsStringAsync();
+                if (responseMessage.Contains("error"))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
-        public async Task<RegisteredResponse> RegisterApp()
+        public async Task<RegisterUserResponse> RegisterApp(RegisterUserRequest request)
         {
-            var content = new StringContent("");
-            var response = await client.PostAsync("/api/register/", content);
-            var responseObject = JsonConvert.DeserializeObject<RegisteredResponse>(await response.Content.ReadAsStringAsync());
+            var content = new StringContent(JsonConvert.SerializeObject(request));
+            var response = await client.PostAsync("/api/", content);
+            var responseObject = JsonConvert.DeserializeObject<RegisterUserResponse>(await response.Content.ReadAsStringAsync());
             return responseObject;
         }
 
