5a1d15859b1e420049d164292de56effc226dcc7
ludvig <cederfelt@live.com>
2017-05-01 14:04:23 +0200

    added xaml cross platform

---
+++ HueIncomingCallCP/HueIncomingCallCP/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using HueIncomingCallCP.Helpers;
+using HueIncomingCallCP.Models;
+using HueIncomingCallCP.Services;
+
+using Xamarin.Forms;
+
+namespace HueIncomingCallCP.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        /// <summary>
+        /// Get the azure service instance
+        /// </summary>
+        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+        /// <summary>
+        /// Private backing field to hold the title
+        /// </summary>
+        string title = string.Empty;
+        /// <summary>
+        /// Public property to set and get the title of the item
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
+
---
+++ HueIncomingCallCP/HueIncomingCallCP/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using HueIncomingCallCP.ViewModels;
+
+using Xamarin.Forms;
+
+namespace HueIncomingCallCP.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
